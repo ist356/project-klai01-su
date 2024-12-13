dd7a530aae0bac02da2ee96c7c45648694e240e7
klai01-su <klai01@syr.edu>
2024-12-13 17:52:31 -0500

    commit

--- code/test2.py
+++ code/test2.py
@@ -1,4 +1,6 @@
 from test import get_account, get_summoner, get_league
+from PIL import Image
+import os
 
 key = "RGAPI-c6d36bf4-db17-48f2-a385-0d35396d2c24"
 
@@ -20,6 +22,12 @@ print("")
 print(league)
 
 print(account['gameName']+account['tagLine'])
-print(summoner['profileIconId'])
+profile_icon_id = summoner['profileIconId']
+
+PROFILE_ICON_PATH = "/data/dragontail-14.24.1/14.21.1/img/profileicon/"
+profile_icon_path = os.path.join(PROFILE_ICON_PATH, f"{profile_icon_id}.png")
+image = Image.open(f'{profile_icon_path}')
+image.show()
+
 print(summoner['summonerLevel'])
 print(league[0]['tier'] + " " + league[0]['rank'] + " " + str(league[0]['leaguePoints']) + " LP")
---
+++ code/testing.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+'''
+profile_icon_id = "300"
+
+PROFILE_ICON_PATH = "../project-klai01-su/data/dragontail-14.24.1/14.21.1/img/profileicon/"
+profile_icon_path = os.path.join(PROFILE_ICON_PATH, f"{profile_icon_id}.png")
+print(profile_icon_path)
+image = Image.open(f'{profile_icon_path}')
+image.show()
+'''
+
+# Define the path to the image (adjust the path for Mac)
+image_path = '..project-klai01-su/data/dragontail-14.24.1/14.21.1/img/profileicon/0.png'
+
+image = Image.open(image_path)
+
+image.show()
